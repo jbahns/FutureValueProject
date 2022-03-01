20ea774113e37bfb229cb0c4606753aea16a88b2
jbahns <javibahns@gmail.com>
2022-03-01 10:34:50 -0500

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author max-student
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+
+	}
+
+}
